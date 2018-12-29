9293864cde3e65694ffa12a82c40625fa868c529
Arnold van der Meulen <arnold@roqua.nl>
2018-12-29 03:13:27 +0100

    Solve day 8b

--- lib/aoc/day8.ex
+++ lib/aoc/day8.ex
@@ -33,20 +33,25 @@ defmodule Aoc2018.Day8b do
     |> Enum.sum
   end
 
+  defp next_node([0, no_meta | input], meta) do
+    {new_meta, rest} = Enum.split(input, no_meta)
+    sum = Enum.sum(new_meta)
+    {rest, meta ++ [sum]}
+  end
   defp next_node([no_nodes, no_meta | input], meta) do
-    # IO.inspect({no_nodes, no_meta, input, meta})
     {rest, new_meta} =
       List.duplicate(1, no_nodes)
       |> Enum.reduce({input, []}, fn _, {rest, acc} ->
         next_node(rest, acc)
       end)
 
-    {new_new_meta, rest} = Enum.split(rest, no_meta)
-    {rest, meta ++ new_meta ++ new_new_meta}
+    {meta_idxs, rest} = Enum.split(rest, no_meta)
+    new_new_meta = Enum.reduce(meta_idxs, 0, fn idx, acc -> acc + Enum.at(new_meta, idx - 1, 0) end)
+    {rest, meta ++ [new_new_meta]}
   end
 
   defp read_input do
-    File.read!("priv/fixtures/day8example.txt")
+    File.read!("priv/fixtures/day8.txt")
     |> String.split(~r/\W/, trim: true)
     |> Enum.map(&String.to_integer/1)
   end
